4424bdb623ffae1cbaf95ff2bc8f510baf1bf48b
Peter Ahlgren <ahlgren1234@gmail.com>
2022-12-26 22:54:07 +0100

    Add start/stop & reset button

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:stopwatch/ui/reset_button.dart';
+import 'package:stopwatch/ui/start_stop_button.dart';
 import 'package:stopwatch/ui/stopwatch_renderer.dart';
 // import 'package:stopwatch/ui/elapsed_time_text_basic.dart';
 import 'dart:async';
@@ -12,7 +14,10 @@ class Stopwatch extends StatefulWidget {
 
 class _StopwatchState extends State<Stopwatch>
     with SingleTickerProviderStateMixin {
-  Duration _elapsed = Duration.zero;
+  Duration _previouslyElapsed = Duration.zero;
+  Duration _currentlyElapsed = Duration.zero;
+  Duration get _elapsed => _previouslyElapsed + _currentlyElapsed;
+  bool _isRunning = false;
   late final Ticker _ticker;
 
   @override
@@ -20,10 +25,9 @@ class _StopwatchState extends State<Stopwatch>
     super.initState();
     _ticker = this.createTicker((elapsed) {
       setState(() {
-        _elapsed = elapsed;
+        _currentlyElapsed = elapsed;
       });
     });
-    _ticker.start();
   }
 
   @override
@@ -32,15 +36,62 @@ class _StopwatchState extends State<Stopwatch>
     super.dispose();
   }
 
+  void _toggleRunning() {
+    setState(() {
+      _isRunning = !_isRunning;
+      if (_isRunning) {
+        _ticker.start();
+      } else {
+        _ticker.stop();
+        _previouslyElapsed += _currentlyElapsed;
+        _currentlyElapsed = Duration.zero;
+      }
+    });
+  }
+
+  void _reset() {
+    _ticker.stop();
+    setState(() {
+      _isRunning = false;
+      _previouslyElapsed = Duration.zero;
+      _currentlyElapsed = Duration.zero;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
         final radius = constraints.maxWidth / 2;
 
-        return StopwatchRenderer(
-          elapsed: _elapsed,
-          radius: radius,
+        return Stack(
+          children: [
+            StopwatchRenderer(
+              elapsed: _elapsed,
+              radius: radius,
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: SizedBox(
+                width: 80,
+                height: 80,
+                child: ResetButton(
+                  onPressed: _reset,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SizedBox(
+                width: 80,
+                height: 80,
+                child: StartStopButton(
+                  isRunning: _isRunning,
+                  onPressed: _toggleRunning,
+                ),
+              ),
+            ),
+          ],
         );
       },
     );
